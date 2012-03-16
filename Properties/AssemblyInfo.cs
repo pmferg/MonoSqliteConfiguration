372d1b403f9677f156f9b32b5f25412cc55dbd00
Paul Ferguson <paul.ferguson@gmail.com>
2012-03-16 01:01:38 +0000

    first project commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FluentNHibernate.MonoContrib")]
+[assembly: AssemblyDescription("Allows Fluent NHibernate to use SQLite under mono")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Paul Ferguson")]
+[assembly: AssemblyProduct("FluentNHibernate.MonoContrib")]
+[assembly: AssemblyCopyright("Copyright ©  2012 Paul Ferguson")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("979b0d70-7068-47a7-9091-26d2ca46eeee")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
